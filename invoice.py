d954a7a18451c176ef8aa9ca01929f86baf0dae8
Deepanshu Kacher <kacher.deepanshu895@gmail.com>
2024-09-08 06:58:08 +0530

    small changes

--- invoice.py
+++ invoice.py
@@ -1,58 +1,43 @@
-import win32print
-# import asyncio
-# import websockets
+import platform
+
+# Determine the operating system
+current_os = platform.system()
+
+# Conditional import based on the operating system
+if current_os == 'Windows':
+    import win32print
+    # Add any other Windows-specific imports
+elif current_os == 'Linux':
+    # Import Linux-specific printing libraries if needed
+    pass
+elif current_os == 'Darwin':  # macOS
+    # Import macOS-specific printing libraries if needed
+    pass
+else:
+    raise OSError("Unsupported Operating System")
 
 def format_integer_string_back_whitespace(n):
-    # Convert integer to string
     str_n = str(n)
-    
-    # Check if length is less than 7
-    if len(str_n) < 7:
-        # Add whitespace at the end to make it 7 characters long
-        formatted_string = str_n.ljust(7)
-    else:
-        # If length is already 7 or more, keep the string as it is
-        formatted_string = str_n
-    
-    return formatted_string
+    return str_n.ljust(7) if len(str_n) < 7 else str_n
 
 def make_40_characters_long(input_string):
-    # Check if input string length is less than 40
     if len(input_string) < 40:
-        # Calculate the required whitespace on both sides
         total_spaces = 40 - len(input_string)
         left_spaces = total_spaces // 2
-        right_spaces = total_spaces - left_spaces
-
-        # Center the string with whitespace on both sides
-        # result_string = input_string.center(40 - right_spaces, ' ')
-        result_string = (' '*left_spaces) + input_string
+        return (' ' * left_spaces) + input_string
     else:
-        # If length is already 40 or more, keep the string as it is
-        result_string = input_string[:40]  # Trim the string to 40 characters
-
-    return result_string
-
+        return input_string[:40]
 
 def print_with_margins_and_cut(printer_name, itemsArr, top_margin=0, bottom_margin=5):
-
-
-    printer = win32print.OpenPrinter(printer_name)
- 
-# Item----------Qyt-----------RATE---------Amount-    48
-
-# ----------------------------------------    40
-# Item      Qyt         RATE        Amount    40
-
+    if current_os == 'Windows':
+        printer = win32print.OpenPrinter(printer_name)
+    # Add logic for Linux/macOS when implemented
+    
     textHead_array = ['Retail invoice','DUTT GURUKARIPA','SOUTH TUKOGANJ','INDORE','Tel:- (0731 4222227)']
 
     processedText = ''
-
     for index, string in enumerate(textHead_array):
-        if index != len(textHead_array) - 1:
-            processedText = processedText + make_40_characters_long(string) + '\n'
-        else:
-            processedText += make_40_characters_long(string)
+        processedText += make_40_characters_long(string) + '\n' if index != len(textHead_array) - 1 else make_40_characters_long(string)
 
     heading = """
 ----------------------------------------
@@ -70,123 +55,61 @@ Total:-               Rs:-  {total_price}
 Thank you for your purchase!
 """
     new_footer = footer + '\n' * bottom_margin
-
-    newHeading = processedText+heading
-
+    newHeading = processedText + heading
     totalPrice = 0
 
     try:
-        
-       h_printer = win32print.GetDefaultPrinter()
-       raw_data = newHeading.encode('utf-8')  # Convert text to bytes
-       h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-       win32print.StartPagePrinter(printer)
-       win32print.WritePrinter(printer, raw_data)
-       win32print.EndPagePrinter(printer)
-       win32print.EndDocPrinter(printer)
-       # Send command for paper cutting
-       for item in itemsArr:
+        if current_os == 'Windows':
+            raw_data = newHeading.encode('utf-8')
+            h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
+            win32print.StartPagePrinter(printer)
+            win32print.WritePrinter(printer, raw_data)
+            win32print.EndPagePrinter(printer)
+            win32print.EndDocPrinter(printer)
 
-
-        printItem = """
+            for item in itemsArr:
+                printItem = """
 {item_name}
           {item_quantity}     {item_price}     {calculate_price}
-"""        
-        calculateRow = item['quantity']*item['price']
+"""
+                calculateRow = item['quantity'] * item['price']
+                puttingValuesinprintItem = printItem.format(
+                    item_name=item['name'],
+                    item_quantity=format_integer_string_back_whitespace(item['quantity']),
+                    item_price=format_integer_string_back_whitespace(item['price']),
+                    calculate_price=calculateRow
+                )
+                totalPrice += calculateRow
+                raw_data = puttingValuesinprintItem.encode('utf-8')
+                h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
+                win32print.StartPagePrinter(printer)
+                win32print.WritePrinter(printer, raw_data)
+                win32print.EndPagePrinter(printer)
+                win32print.EndDocPrinter(printer)
 
-        puttingValuesinprintItem = printItem.format(
-           item_name = item['name'],
-           item_quantity = format_integer_string_back_whitespace(item['quantity']),
-           item_price = format_integer_string_back_whitespace(item['price']),
-           calculate_price = calculateRow
-        )
-        totalPrice+=calculateRow
+            raw_data = new_footer.format(total_price=totalPrice).encode('utf-8')
+            h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
+            win32print.StartPagePrinter(printer)
+            win32print.WritePrinter(printer, raw_data)
+            win32print.EndPagePrinter(printer)
+            win32print.EndDocPrinter(printer)
 
-        h_printer = win32print.GetDefaultPrinter()
-        raw_data = puttingValuesinprintItem.encode('utf-8')  # Convert text to bytes
-        h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-        win32print.StartPagePrinter(printer)
-        win32print.WritePrinter(printer, raw_data)
-        win32print.EndPagePrinter(printer)
-        win32print.EndDocPrinter(printer)
-       # Send command for paper cutting
-       h_printer = win32print.GetDefaultPrinter()
-       raw_data = new_footer.format(total_price = totalPrice).encode('utf-8')  # Convert text to bytes
-       h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-       win32print.StartPagePrinter(printer)
-       win32print.WritePrinter(printer, raw_data)
-       win32print.EndPagePrinter(printer)
-       win32print.EndDocPrinter(printer)
-       # Send command for paper cutting
-    
     finally:
-        cut_command = b'\x1D\x56\x01'  # ESC/POS command for paper cutting
-        h_job = win32print.StartDocPrinter(printer, 1, ("Cutting Paper", None, "RAW"))
-        win32print.StartPagePrinter(printer)
-        win32print.WritePrinter(printer, cut_command)
-        win32print.EndPagePrinter(printer)
-        win32print.EndDocPrinter(printer)
-        win32print.ClosePrinter(printer)
+        if current_os == 'Windows':
+            cut_command = b'\x1D\x56\x01'  # ESC/POS command for paper cutting
+            h_job = win32print.StartDocPrinter(printer, 1, ("Cutting Paper", None, "RAW"))
+            win32print.StartPagePrinter(printer)
+            win32print.WritePrinter(printer, cut_command)
+            win32print.EndPagePrinter(printer)
+            win32print.EndDocPrinter(printer)
+            win32print.ClosePrinter(printer)
+        # Add paper cutting logic for other OS if needed
 
-# Replace 'RETSOL RTP-80' with the name of your printer
+# Example usage
 # printer_name = 'RETSOL RTP-80'
-
 # itemsArr = [
-#     {"name":'Mongo juice Mongo juice',"quantity":100,"price":2.00},
-#     # {"name":'Mongo juice Mongo juice Mongo juice Mongo juice',"quantity":50,"price":20.00},
-#     # {"name":'Mongo juice Mongo juice Mongo juice Mongo juice Mongo juice Mongo juice',"quantity":5,"price":20.00},
-#     # {"name":'Cow urine juice',"quantity":7,"price":5.00},
+#     {"name":'Mongo juice',"quantity":100,"price":2.00},
 #     {"name":'Laptop',"quantity":15,"price":50.00},
 #     {"name":'Speaker',"quantity":20,"price":700.00},
 # ]
-# print_with_margins_and_cut(printer_name, itemsArr, top_margin=0, bottom_margin=5)
-
-# async def websocket_handler(websocket, path):
-#     # Receive data from the WebSocket connection
-
-#     try:
-#      # Your code goes here
-#         data = await websocket.recv()
-#         print(data)
-#     except Exception as e:
-#          print(f"An error occurred: {e}")
-
-    
-#     # Process the received data and call the print_with_margins_and_cut() function
-#     # with the appropriate arguments
-#     printer_name = "your_printer_name"  # Replace with your desired printer name
-#     itemsArr = []  # Replace with your actual data
-    
-#     # print_with_margins_and_cut(printer_name, itemsArr)
-
-#     # Send a response back to the WebSocket client if needed
-#     await websocket.send("Data received and processed successfully")
-
-# # Add this line inside your print_with_margins_and_cut() function
-# # to establish the WebSocket server and start listening for connections
-# start_server = websockets.serve(websocket_handler, "localhost", 8765)
-
-# # Run the WebSocket server
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_forever()
-
-
-# text = """
-# ----------------------------------------
-#                 INVOICE
-# ----------------------------------------
-# Item           |    Quantity    |  Price
-# ----------------------------------------
-# Item 1        
-#                |       2        |  $10.00
-# Item 2         
-#                |       1        |  $5.00
-# ----------------------------------------
-# Total:                        $15.00
-# ----------------------------------------
-
-# Thank you for your purchase!
-# """
-
-
-
+# print_with_margins_and_cut(printer_name, itemsArr)
--- kotprint.py
+++ kotprint.py
@@ -1,11 +1,11 @@
-import win32print
+import platform
 from datetime import datetime
-from typing import List, Union, TypedDict
-# import asyncio
-# import websockets
+from typing import List, TypedDict
 
-
-
+if platform.system() == "Windows":
+    import win32print
+else:
+    import cups  # For Linux printing
 
 class Item(TypedDict):
     size: str  # Size can be "L", "M", or "S"
@@ -16,57 +16,84 @@ class Item(TypedDict):
     printCount: int  # Print count as a number
     kotId: str  # Kot ID as a string
 
-def format_integer_string_back_whitespace(n,length):
-    # Convert integer to string
+def format_integer_string_back_whitespace(n, length):
     str_n = str(n)
-    
-    # Check if length is less than 7
     if len(str_n) < length:
-        # Add whitespace at the end to make it 7 characters long
         formatted_string = str_n.rjust(length)
     else:
-        # If length is already 7 or more, keep the string as it is
         formatted_string = str_n
-    
     return formatted_string
 
 def make_40_characters_long(input_string):
-    # Check if input string length is less than 40
     if len(input_string) < 40:
-        # Calculate the required whitespace on both sides
         total_spaces = 40 - len(input_string)
         left_spaces = total_spaces // 2
-        right_spaces = total_spaces - left_spaces
-
-        # Center the string with whitespace on both sides
-        # result_string = input_string.center(40 - right_spaces, ' ')
-        result_string = (' '*left_spaces) + input_string
+        result_string = (' ' * left_spaces) + input_string
     else:
-        # If length is already 40 or more, keep the string as it is
-        result_string = input_string[:40]  # Trim the string to 40 characters
-
+        result_string = input_string[:40]
     return result_string
 
-
-def kotPrint(printer_name:str, itemsArr:List[Item], top_margin=0, bottom_margin=5):
-
-
+def print_windows(printer_name, itemsArr, top_margin=0, bottom_margin=5):
     printer = win32print.OpenPrinter(printer_name)
- 
-# Item----------Qyt-----------RATE---------Amount-    48
+    heading = """
+DATE & TIME:- {date}
+KOT NO. :- {kotCount}     PRINT NO.:- {printCount}
+----------------------------------------
+Size    Qyt        Item
+----------------------------------------
+"""
+    newHeading = heading.format(
+        date=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+        kotCount=itemsArr[0]['kotCount'],
+        printCount=itemsArr[0]['printCount']
+    )
 
-# ----------------------------------------    40
-# Item      Qyt         RATE        Amount    40
+    try:
+        raw_data = newHeading.encode('utf-8')
+        h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
+        win32print.StartPagePrinter(printer)
+        win32print.WritePrinter(printer, raw_data)
+        win32print.EndPagePrinter(printer)
+        win32print.EndDocPrinter(printer)
 
-    # textHead_array = ['DUTT GURUKARIPA',]
+        for item in itemsArr:
+            printItem = """{size}    {quantity}   {halfFull}   {dishName}\n"""
+            puttingValuesinprintItem = printItem.format(
+                halfFull=item['halfFull'],
+                quantity=format_integer_string_back_whitespace(item['quantity'], 4),
+                size=item['size'],
+                dishName=item['dishName']
+            )
 
-    # processedText = ''
+            raw_data = puttingValuesinprintItem.encode('utf-8')
+            h_job = win32print.StartDocPrinter(printer, 1, ("Printing Item", None, "RAW"))
+            win32print.StartPagePrinter(printer)
+            win32print.WritePrinter(printer, raw_data)
+            win32print.EndPagePrinter(printer)
+            win32print.EndDocPrinter(printer)
 
-    # for index, string in enumerate(textHead_array):
-    #     if index != len(textHead_array) - 1:
-    #         processedText = processedText + make_40_characters_long(string) + '\n'
-    #     else:
-    #         processedText += make_40_characters_long(string)
+        raw_data = ('\n' * bottom_margin).encode('utf-8')
+        h_job = win32print.StartDocPrinter(printer, 1, ("Printing Margins", None, "RAW"))
+        win32print.StartPagePrinter(printer)
+        win32print.WritePrinter(printer, raw_data)
+        win32print.EndPagePrinter(printer)
+        win32print.EndDocPrinter(printer)
+    finally:
+        cut_command = b'\x1D\x56\x01'
+        h_job = win32print.StartDocPrinter(printer, 1, ("Cutting Paper", None, "RAW"))
+        win32print.StartPagePrinter(printer)
+        win32print.WritePrinter(printer, cut_command)
+        win32print.EndPagePrinter(printer)
+        win32print.EndDocPrinter(printer)
+        win32print.ClosePrinter(printer)
+
+    return itemsArr[0]['kotId']
+
+def print_linux(printer_name, itemsArr, top_margin=0, bottom_margin=5):
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+    if printer_name not in printers:
+        raise Exception(f"Printer '{printer_name}' not found.")
 
     heading = """
 DATE & TIME:- {date}
@@ -75,77 +102,26 @@ KOT NO. :- {kotCount}     PRINT NO.:- {printCount}
 Size    Qyt        Item
 ----------------------------------------
 """
-
-#  
-#    
-
-
     newHeading = heading.format(
-        date = datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-        kotCount = itemsArr[0]['kotCount'],
-        printCount = itemsArr[0]['printCount']
+        date=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+        kotCount=itemsArr[0]['kotCount'],
+        printCount=itemsArr[0]['printCount']
     )
 
+    job_name = "KOT Printing"
+    for item in itemsArr:
+        printItem = """{size}    {quantity}   {halfFull}   {dishName}\n"""
+        puttingValuesinprintItem = printItem.format(
+            halfFull=item['halfFull'],
+            quantity=format_integer_string_back_whitespace(item['quantity'], 4),
+            size=item['size'],
+            dishName=item['dishName']
+        )
+        content = newHeading + puttingValuesinprintItem + '\n' * bottom_margin
+        conn.printFile(printer_name, content, job_name, {})
 
-    try:
-        
-        h_printer = win32print.GetDefaultPrinter()
-        raw_data = newHeading.encode('utf-8')  # Convert text to bytes
-        h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-        win32print.StartPagePrinter(printer)
-        win32print.WritePrinter(printer, raw_data)
-        win32print.EndPagePrinter(printer)
-        win32print.EndDocPrinter(printer)
-        # Send command for paper cutting
-        for item in itemsArr:
-
-
-            printItem = """{size}    {quantity}   {halfFull}   {dishName}"""+"\n"
-
-            puttingValuesinprintItem = printItem.format(
-               halfFull = item['halfFull'],
-               quantity = format_integer_string_back_whitespace(item['quantity'],4),
-               size = item['size'],
-               dishName=item['dishName']
-            )
-            
-            h_printer = win32print.GetDefaultPrinter()
-            raw_data = puttingValuesinprintItem.encode('utf-8')  # Convert text to bytes
-            h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-            win32print.StartPagePrinter(printer)
-            win32print.WritePrinter(printer, raw_data)
-            win32print.EndPagePrinter(printer)
-            win32print.EndDocPrinter(printer)    
-
-
-
-        h_printer = win32print.GetDefaultPrinter()
-        raw_data = (''+ '\n' * bottom_margin).encode('utf-8')  # Convert text to bytes
-        h_job = win32print.StartDocPrinter(printer, 1, ("Printing with Margins and Cutting", None, "RAW"))
-        win32print.StartPagePrinter(printer)
-        win32print.WritePrinter(printer, raw_data)
-        win32print.EndPagePrinter(printer)
-        win32print.EndDocPrinter(printer)
-    finally:
-
-        cut_command = b'\x1D\x56\x01'  # ESC/POS command for paper cutting
-        h_job = win32print.StartDocPrinter(printer, 1, ("Cutting Paper", None, "RAW"))
-        win32print.StartPagePrinter(printer)
-        win32print.WritePrinter(printer, cut_command)
-        win32print.EndPagePrinter(printer)
-        win32print.EndDocPrinter(printer)
-        win32print.ClosePrinter(printer)
-
-
-        return itemsArr[0]['kotId']
-
-
-
-
-
-""" {
-    size: Order['size']
-    quantity: number
-    halfFull: "H" | "F"
-    dishName: string
-} """
+def kotPrint(printer_name: str, itemsArr: List[Item], top_margin=0, bottom_margin=5):
+    if platform.system() == "Windows":
+        return print_windows(printer_name, itemsArr, top_margin, bottom_margin)
+    else:
+        return print_linux(printer_name, itemsArr, top_margin, bottom_margin)
